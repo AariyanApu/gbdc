ceabc56a20537182f4f897076b04ecee94e28ea1
Aariyan Apu <aariyan.apu@gmail.com>
2023-01-25 21:51:53 +0600

    first commit after delete unused code

---
+++ src/app/breaking/page.js
@@ -0,0 +1,15 @@
+'use client';
+
+import Link from 'next/link';
+
+export default function Page() {
+  return (
+    <div>
+      <div>BREAKING</div>
+      <div>
+       
+        <Link href="/second">Move to Second</Link>
+      </div>
+    </div>
+  );
+}
