66c74428f0bc5a207f20e233f2683a76aaf83902
Aariyan Apu <aariyan.apu@gmail.com>
2023-01-26 00:09:29 +0600

    navbar and logo added

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,7 +1,108 @@
 'use client';
 
+import { Disclosure } from "@headlessui/react";
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline';
+import Image from "next/image";
+
+
+
+const navigation = [
+  { name: 'ABOUT US', href: '#', current: false },
+  { name: 'ACTIVITIS', href: '#', current: false },
+  { name: 'ACHIVMENT', href: '#', current: false },
+  { name: 'TESTIMONIAL', href: '#', current: false },
+  { name: 'FEATURE', href: '#', current: false },
+  { name: 'ADMIN PANEL', href: '#', current: false },
+];
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(' ');
+}
+
 export default function NavBar() {
-  return (
-    <div>Navbar</div>
-  )
+  return <>
+
+
+    <Disclosure as="nav" className="bg-violet-500">
+      {({ open }) => (
+        <>
+          <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+            <div className="relative flex h-16 items-center justify-between">
+              <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+                {/* Mobile menu button*/}
+                <Disclosure.Button className="inline-flex items-center justify-center rounded-md p-2 text-gray-50 hover:bg-violet-600 hover:text-gray-50 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-violet-600">
+                  <span className="sr-only">Open main menu</span>
+                  {open ? (
+                    <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
+                  ) : (
+                    <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
+                  )}
+                </Disclosure.Button>
+              </div>
+              <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-between">
+                <div className="flex flex-shrink-0 items-center">
+                  <Image
+                    className="block h-8 w-auto lg:hidden"
+                    src={'/logo.png'}
+                    alt="Logo of Aaru"
+                    width={100}
+                    height={100}
+                  />
+                  <Image
+                    className="hidden h-8 w-auto lg:block"
+                    src={'/logo.png'}
+                    alt="Logo of Aaru"
+                    width={100}
+                    height={100}
+                  />
+                </div>
+                <div className="hidden sm:ml-6 sm:block">
+                  <div className="flex space-x-4">
+                    {navigation.map((item) => (
+                      <a
+                        key={item.name}
+                        href={item.href}
+                        className={classNames(
+                          item.current
+                            ? 'bg-violet-500 text-gray-50'
+                            : 'text-gray-50 hover:bg-violet-600 ',
+                          'rounded-md px-3 py-2 text-sm font-medium',
+                        )}
+                        aria-current={item.current ? 'page' : undefined}
+                      >
+                        {item.name}
+                      </a>
+                    ))}
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <Disclosure.Panel className="sm:hidden">
+            <div className="space-y-1 px-2 pt-2 pb-3">
+              {navigation.map((item) => (
+                <Disclosure.Button
+                  key={item.name}
+                  as="a"
+                  href={item.href}
+                  className={classNames(
+                    item.current
+                      ? 'bg-violet-600 text-gray-50'
+                      : 'text-gray-50 hover:bg-violet-600 ',
+                    'block rounded-md px-3 py-2 text-base font-medium',
+                  )}
+                  aria-current={item.current ? 'page' : undefined}
+                >
+                  {item.name}
+                </Disclosure.Button>
+              ))}
+            </div>
+          </Disclosure.Panel>
+        </>
+      )}
+    </Disclosure>
+
+
+  </>;
 }
