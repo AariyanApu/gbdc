dabb3be094a0179a30c48dd39443e0529b352503
Aariyan Apu <aariyan.apu@gmail.com>
2023-02-16 17:08:43 +0600

    fix banner width

--- src/assets/videoDetails.js
+++ src/assets/videoDetails.js
@@ -1,35 +1,35 @@
-import poster1 from './banner1.jpg';
-import poster2 from './banner2.jpg';
-import poster3 from './banner3.jpg';
+import banner1 from './banner1.jpg';
+import banner2 from './banner2.jpg';
+import banner3 from './banner3.jpg';
+import banner4 from './banner4.jpg';
 
-
-
+// eslint-disable-next-line import/no-anonymous-default-export
 export default [
   {
     title: 'Avatar 2',
 
-    imgUrl: poster1,
+    imgUrl: banner1,
     description:
       'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam quae consequuntur facere animi labore unde optio recusandae doloribus suscipit! Similique, neque. Similique laboriosam earum repellendus atque voluptates, assumenda animi nulla voluptas dolores nam? Porro culpa natus, quod atque velit numquam.',
   },
-    {
+  {
     title: 'Avatar 3',
 
-    imgUrl: poster2,
+    imgUrl: banner2,
     description:
       'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam quae consequuntur facere animi labore unde optio recusandae doloribus suscipit! Similique, neque. Similique laboriosam earum repellendus atque voluptates, assumenda animi nulla voluptas dolores nam? Porro culpa natus, quod atque velit numquam.',
   },
-    {
+  {
     title: 'Avatar magic 2',
 
-    imgUrl: poster3,
+    imgUrl: banner3,
     description:
       'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam quae consequuntur facere animi labore unde optio recusandae doloribus suscipit! Similique, neque. Similique laboriosam earum repellendus atque voluptates, assumenda animi nulla voluptas dolores nam? Porro culpa natus, quod atque velit numquam.',
   },
-    {
+  {
     title: 'Avatar Here another 2',
 
-    imgUrl: poster3,
+    imgUrl: banner4,
     description:
       'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam quae consequuntur facere animi labore unde optio recusandae doloribus suscipit! Similique, neque. Similique laboriosam earum repellendus atque voluptates, assumenda animi nulla voluptas dolores nam? Porro culpa natus, quod atque velit numquam.',
   },
--- src/components/Slide.jsx
+++ src/components/Slide.jsx
@@ -1,60 +1,56 @@
 'use client';
+
 import { motion } from 'framer-motion';
-import SlideDetails from '../assets/videoDetails';
-
-import { Swiper, SwiperSlide } from 'swiper/react';
-
-import { Autoplay, EffectFade, Lazy, Pagination } from 'swiper';
-
+import Image from 'next/image';
+import { Autoplay, EffectFade, Pagination } from 'swiper';
 import 'swiper/css/effect-fade';
-import 'swiper/css/lazy';
 import 'swiper/css/pagination';
+import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/swiper-bundle.min.css';
 import 'swiper/swiper.min.css';
+import SlideDetails from '../assets/videoDetails';
 
-export default function Slide() {
-  return (
-    <>
-      <Swiper
-        effect={'fade'}
-        grabCursor={true}
-        centeredSlides={true}
-        slidesPerView={'auto'}
-        autoplay={{
-          delay: 7000,
-          disableOnInteraction: false,
-          pauseOnMouseEnter: true,
-        }}
-        loop={true}
-        loopFillGroupWithBlank={true}
-        lazy={true}
-        pagination={{ clickable: true, dynamicBullets: true }}
-        modules={[EffectFade, Pagination, Autoplay, Lazy]}
-        className="mySwiper w-full px-2  pb-10 mt-0.5"
-      >
-        {SlideDetails?.map((i, idx) => (
-          <SwiperSlide key={idx}>
-            <div className="relative h-[540px]">
-              <img
-                src={'/banner3.jpg'}
-                className="swiper-lazy h-[540px] absolute object-cover rounded w-full drop-shadow-xl "
-              />
+const Slide = () => (
+  <Swiper
+    effect="fade"
+    grabCursor
+    centeredSlides
+    slidesPerView="auto"
+    autoplay={{
+      delay: 7000,
+      disableOnInteraction: false,
+      pauseOnMouseEnter: true,
+    }}
+    loop
+    loopFillGroupWithBlank
+    pagination={{ clickable: true, dynamicBullets: true }}
+    modules={[EffectFade, Pagination, Autoplay]}
+    className="mySwiper mt-0.5 mb-10 w-full "
+  >
+    {/* Here Swiper Card goes on */}
+    {SlideDetails?.map((i, idx) => (
+      <SwiperSlide key={idx}>
+        <div className="relative h-[540px]  p-5">
+          <Image
+            alt="banner Image"
+            height={1000}
+            width={1000}
+            src={i.imgUrl}
+            className=" h-[510px] w-full rounded object-cover drop-shadow-lg "
+          />
 
-              <motion.div
-                animate={{ y: 100 }}
-                transition={{ ease: 'easeOut', duration: 1 }}
-                className="absolute inset-60  text-center  text-white"
-              >
-                <div className="text-4xl bg-black bg-opacity-5">{i.title}</div>
-                <div className="text-lg bg-black bg-opacity-5">
-                  {i.description}
-                </div>
-              </motion.div>
-            </div>
-            <div className="swiper-lazy-preloader swiper-lazy-preloader-white"></div>
-          </SwiperSlide>
-        ))}
-      </Swiper>
-    </>
-  );
-}
+          <motion.div
+            animate={{ y: 100 }}
+            transition={{ ease: 'easeOut', duration: 1 }}
+            className="absolute inset-60  text-center  text-white"
+          >
+            <div className="text-4xl">{i.title}</div>
+            <div className="text-lg">{i.description}</div>
+          </motion.div>
+        </div>
+      </SwiperSlide>
+    ))}
+  </Swiper>
+);
+
+export default Slide;
