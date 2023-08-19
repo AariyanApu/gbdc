110f1bfb2a9a79d8221d146a47f21d2e8d56991e
Aariyan Apu <38152118+AariyanApu@users.noreply.github.com>
2023-08-19 12:26:12 +0600

    sliced counter card

---
+++ src/components/CounterCard.tsx
@@ -0,0 +1,46 @@
+import { titleFont } from "@/utils/fonts";
+import Image from "next/image";
+import CountUp, { useCountUp } from "react-countup";
+
+interface counterCardProps {
+  end: number;
+  imgSrc: string;
+  desc: string;
+}
+
+export default function CounterCard({
+  end,
+  imgSrc,
+
+  desc,
+}: counterCardProps) {
+  useCountUp({
+    ref: "counter",
+    end: 1234567,
+    enableScrollSpy: true,
+    scrollSpyDelay: 1000,
+  });
+  return (
+    <CountUp enableScrollSpy start={0} end={end} delay={0} duration={2.75}>
+      {({ countUpRef }) => (
+        <div
+          className={`${titleFont.className} item-center mx-auto mt-10 flex w-[300px] flex-col justify-center gap-y-1 rounded-md bg-slate-200 p-10 text-center shadow-md`}
+        >
+          <Image
+            src={imgSrc}
+            height={200}
+            width={200}
+            alt={desc}
+            className="mx-auto mb-2 w-32 drop-shadow-md "
+          />
+          <div className="mx-auto flex gap-x-1">
+            <span className="text-7xl text-red-700" ref={countUpRef} />
+
+            <span className="text-7xl text-red-700">+</span>
+          </div>
+          <span className="text-2xl ">{desc}</span>
+        </div>
+      )}
+    </CountUp>
+  );
+}
--- src/sections/Counter.tsx
+++ src/sections/Counter.tsx
@@ -1,19 +1,11 @@
 "use client";
 
+import CounterCard from "@/components/CounterCard";
 import { TypingTitle } from "@/components/CustomText";
-import { titleFont } from "@/utils/fonts";
 import { staggerContainer } from "@/utils/motion";
 import { motion } from "framer-motion";
-import Image from "next/image";
-import CountUp, { useCountUp } from "react-countup";
 
 export default function Counter() {
-  useCountUp({
-    ref: "counter",
-    end: 1234567,
-    enableScrollSpy: true,
-    scrollSpyDelay: 1000,
-  });
   return (
     <motion.div
       className=" my-16 md:my-20 lg:my-20 xl:my-28 2xl:my-32 "
@@ -23,24 +15,8 @@ export default function Counter() {
       viewport={{ once: true, amount: 0.25 }}
     >
       <TypingTitle title={"We Have Arranged "} />
-      <CountUp enableScrollSpy start={0} end={800} delay={0} duration={2.75}>
-        {({ countUpRef }) => (
-          <div className={`${titleFont.className}`}>
-            <Image
-              src="/assets/bloodBags.png"
-              height={100}
-              width={100}
-              alt="Blood Bags"
-            />
-            <div className="flex gap-x-1">
-              <span className="text-7xl text-red-700" ref={countUpRef} />
 
-              <span className="text-7xl text-red-700">+</span>
-            </div>
-            <span className="text-2xl ">Bags Blood</span>
-          </div>
-        )}
-      </CountUp>
+      <CounterCard end={800} imgSrc="/assets/bloodBags.png" desc="Bags Blood" />
     </motion.div>
   );
 }
