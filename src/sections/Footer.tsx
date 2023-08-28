2c17d8b5ba1627c388c13e0a00efd495cea6ec23
Aariyan Apu <38152118+AariyanApu@users.noreply.github.com>
2023-08-29 01:08:52 +0600

    change footer link style

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -27,7 +27,7 @@ export default function Footer() {
             <a
               key={index}
               href={link.href}
-              className="w-44 rounded-md border border-slate-200 px-4 py-2 text-center text-sm transition-colors duration-500 ease-in-out hover:border-sky-500 hover:text-sky-500"
+              className="w-44 rounded-md border border-slate-200/70 px-4 py-2 text-center text-sm  transition-colors duration-500 ease-in-out hover:border-sky-500 hover:text-sky-500 hover:shadow-md"
             >
               {link.name}
             </a>
@@ -38,7 +38,7 @@ export default function Footer() {
             <a
               key={index}
               href={link.href}
-              className="w-44 rounded-md border border-slate-200 px-4 py-2 text-center text-sm transition-colors duration-500 ease-in-out hover:border-sky-500 hover:text-sky-500"
+              className="w-44 rounded-md border border-slate-200/70 px-4 py-2 text-center text-sm transition-colors duration-500 ease-in-out hover:border-sky-500 hover:text-sky-500 hover:shadow-md"
             >
               {link.name}
             </a>
