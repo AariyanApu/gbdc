ec6cac725e819c8e568a64d51ed77edcb1fd2f85
Aariyan Apu <aariyan.apu@gmail.com>
2023-09-29 14:54:40 +0600

    update feature

--- src/sections/Featured.tsx
+++ src/sections/Featured.tsx
@@ -1,20 +1,11 @@
-'use client'
-
+import Container from '@/components/Container'
 import { TypingTitle } from '@/components/CustomText'
 import FeaturedCard from '@/components/FeaturedCard'
 import { featuredData } from '@/utils/data'
-import { staggerContainer } from '@/utils/motion'
-import { motion } from 'framer-motion'
 
 export default function Featured() {
   return (
-    <motion.div
-      className=" my-16  xl:my-20 2xl:my-20 "
-      variants={staggerContainer}
-      initial="hidden"
-      whileInView="show"
-      viewport={{ once: true, amount: 0.25 }}
-    >
+    <Container customStyle=" my-16  xl:my-20 2xl:my-20 ">
       <TypingTitle title={' We are featured on'} />
 
       <div className="mt-8 flex flex-col items-center justify-center gap-x-4 gap-y-3 px-4  md:mt-10 md:flex-row lg:pb-20">
@@ -22,6 +13,6 @@ export default function Featured() {
           <FeaturedCard key={index} imgUrl={item.imgUrl} link={item.link} />
         ))}
       </div>
-    </motion.div>
+    </Container>
   )
 }
