fab94d654dabc33e17a3282b8c529e4c15f83889
Aariyan Apu <38152118+AariyanApu@users.noreply.github.com>
2023-11-04 21:35:47 +0600

    fetch achievements data

--- src/app/achievements/page.tsx
+++ src/app/achievements/page.tsx
@@ -1,28 +1,32 @@
 import Container from '@/components/Container'
 import { TypingTitle } from '@/components/CustomText'
-import { achievementsData } from '@/utils/data'
+import { activitiesData } from '@/types/randomTypes'
+import { getDataNoStore } from '@/utils/getData'
+// import { achievementsData } from '@/utils/data'
 import Image from 'next/image'
-import React from 'react'
 
-export default function Achievements() {
+export default async function Achievements() {
+  const achievementsData: activitiesData[] = await getDataNoStore(
+    'achievements',
+  )
   return (
     <Container customStyle=" my-8 sm:my-16 max-w-7xl mx-auto">
       <TypingTitle title={'Our Achievements'} />
 
       <div className="mt-8 flex flex-row flex-wrap items-center justify-center gap-4">
-        {achievementsData?.map((item, idx) => (
+        {achievementsData?.map((item) => (
           <div
-            key={idx}
+            key={item.id}
             className="flex w-96 flex-col items-center rounded-lg bg-slate-300 text-center "
           >
             <Image
-              src={item.src}
+              src={item.imgUrl}
               alt="donate blood"
               width={500}
               height={500}
               className="h-96 w-96 rounded-tl-lg rounded-tr-lg object-cover object-center"
             />
-            <div className="py-4">We donate Blood regularly</div>
+            <div className="py-4">{item.title}</div>
           </div>
         ))}
       </div>
--- src/app/api/achievements/route.ts
+++ src/app/api/achievements/route.ts
@@ -3,8 +3,8 @@ import { NextResponse } from 'next/server'
 
 export const GET = async () => {
   try {
-    const achievements = await prisma.achievements.findMany()
-    return new NextResponse(JSON.stringify(achievements), { status: 200 })
+    const anothers = await prisma.another.findMany()
+    return new NextResponse(JSON.stringify(anothers), { status: 200 })
   } catch (error) {
     return new NextResponse('Database Error', { status: 500 })
   }
