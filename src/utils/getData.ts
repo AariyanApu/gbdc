88b84d5169a93c418cf138a39ef79227d2f9234e
Aariyan Apu <38152118+AariyanApu@users.noreply.github.com>
2023-11-05 22:28:19 +0600

    fix fetching bug

--- src/utils/getData.ts
+++ src/utils/getData.ts
@@ -13,14 +13,14 @@
 //   };
 // }
 export const getData = async (id: string) => {
-  const res = await fetch(`http://localhost:3000/api/${id}`)
+  const res = await fetch(`https://gbdc.vercel.app/api/${id}`)
   if (!res.ok) {
     throw new Error('Failed to fetch data')
   }
   return res.json()
 }
 export const getDataNoStore = async (id: string) => {
-  const res = await fetch(`http://localhost:3000/api/${id}`, {
+  const res = await fetch(`https://gbdc.vercel.app/api/${id}`, {
     cache: 'no-store',
   })
   if (!res.ok) {
