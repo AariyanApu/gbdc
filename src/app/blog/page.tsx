2e398c319d0434b2b683752db0b4ba1ae682785a
Aariyan Apu <38152118+AariyanApu@users.noreply.github.com>
2023-11-11 00:06:51 +0600

    added transiation to  blogpost

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -2,8 +2,12 @@ import BlogPostCard from '@/components/BlogPostCard'
 
 export default function Blog() {
   return (
-    <div className="mx-auto my-6 max-w-5xl">
+    <div className="mx-auto my-6 flex max-w-5xl flex-col gap-y-4">
       {/* Blog Post Card */}
+      <BlogPostCard />
+      <BlogPostCard />
+      <BlogPostCard />
+      <BlogPostCard />
       <BlogPostCard />
     </div>
   )
--- src/components/BlogPostCard.tsx
+++ src/components/BlogPostCard.tsx
@@ -1,14 +1,18 @@
 import Image from 'next/image'
+import Link from 'next/link'
 
 export default function BlogPostCard() {
   return (
-    <div className="flex flex-row justify-around rounded-md bg-slate-200/80">
+    <Link
+      href={'/'}
+      className="flex flex-row justify-around rounded-md bg-slate-200/70 transition-colors duration-300 ease-in-out hover:bg-slate-200"
+    >
       <Image
         src={'/assets/about.webp'}
         alt=""
         height={500}
         width={500}
-        className="h-72 w-[450px] rounded-s-md object-cover"
+        className="h-72 w-[450px] rounded-s-md object-cover opacity-95 transition-opacity duration-300 ease-in-out hover:opacity-100"
       />
       <div className=" flex flex-col gap-y-2 pl-8">
         <p className="mt-4 text-sm">11/11/2023</p>
@@ -23,6 +27,6 @@ export default function BlogPostCard() {
           similique assumenda, corrupti obcaecati?
         </p>
       </div>
-    </div>
+    </Link>
   )
 }
