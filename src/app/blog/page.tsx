2756150a93d6cd75d0c5634cfe1e3ed0264307ef
Aariyan Apu <38152118+AariyanApu@users.noreply.github.com>
2023-11-11 00:26:00 +0600

    make blogpost responsive

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -2,7 +2,7 @@ import BlogPostCard from '@/components/BlogPostCard'
 
 export default function Blog() {
   return (
-    <div className="mx-auto my-6 flex max-w-5xl flex-col gap-y-4">
+    <div className="mx-auto my-6 flex max-w-5xl flex-col gap-y-8 sm:gap-y-4">
       {/* Blog Post Card */}
       <BlogPostCard />
       <BlogPostCard />
--- src/components/BlogPostCard.tsx
+++ src/components/BlogPostCard.tsx
@@ -5,22 +5,22 @@ export default function BlogPostCard() {
   return (
     <Link
       href={'/'}
-      className="flex flex-row justify-around rounded-md bg-slate-200/70 transition-colors duration-300 ease-in-out hover:bg-slate-200"
+      className="mx-4 flex flex-col justify-around rounded-md bg-slate-200/70 transition-colors duration-300 ease-in-out hover:bg-slate-200 sm:flex-row xl:mx-0"
     >
       <Image
         src={'/assets/about.webp'}
         alt=""
         height={500}
         width={500}
-        className="h-72 w-[450px] rounded-s-md object-cover opacity-95 transition-opacity duration-300 ease-in-out hover:opacity-100"
+        className="h-72 w-[450px] rounded-t-md object-cover opacity-95 transition-opacity duration-300 ease-in-out hover:opacity-100 sm:rounded-s-md sm:rounded-tr-none md:h-80"
       />
-      <div className=" flex flex-col gap-y-2 pl-8">
+      <div className=" flex flex-col gap-y-2 pb-1 pl-1 sm:pl-8">
         <p className="mt-4 text-sm">11/11/2023</p>
-        <h1 className="text-3xl font-semibold">
+        <h1 className="text-3xl font-semibold md:text-2xl">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum,
           deserunt!
         </h1>
-        <p className=" ">
+        <p className=" md:text-sm lg:text-base">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum
           asperiores, nam esse quo dolor illum voluptatum possimus quas
           inventore veniam, ipsa eveniet voluptatem! Excepturi dicta itaque
