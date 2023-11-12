cdcf0c9cca040dfff8380abc93cfced2556bb79e
Aariyan Apu <38152118+AariyanApu@users.noreply.github.com>
2023-11-12 20:43:49 +0600

    fix bug

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,45 +1,44 @@
-import BlogPostCard from '@/components/BlogPostCard'
-import CloudImage from '@/components/CloudImage'
-import { getDataNoStore, getDataNoStoreLocal } from '@/utils/getData'
-import Link from 'next/link'
+// import BlogPostCard from '@/components/BlogPostCard'
+// import CloudImage from '@/components/CloudImage'
+// import { getDataNoStore, getDataNoStoreLocal } from '@/utils/getData'
+// import Link from 'next/link'
 
-interface activitiesData {
-  id: number
-  title: string
-  imgUrl: string
-  desc: string
-}
-export default async function Blog() {
-  const data: activitiesData[] = await getDataNoStore('posts')
+// interface activitiesData {
+//   id: number
+//   title: string
+//   imgUrl: string
+//   desc: string
+// }
+// export default async function Blog() {
+//   const data: activitiesData[] = await getDataNoStore('posts')
 
-  return (
-    <div className="mx-auto my-6 flex max-w-5xl flex-col gap-y-8 sm:gap-y-4">
-      {/* Blog Post Card */}
+//   return (
+//     <div className="mx-auto my-6 flex max-w-5xl flex-col gap-y-8 sm:gap-y-4">
+//       {/* Blog Post Card */}
 
-      {Array.isArray(data) &&
-        data?.map((item: activitiesData) => (
-          <Link
-            key={item.id}
-            href={'/blog/slug'}
-            className="mx-4 flex flex-col justify-around rounded-md bg-slate-200/70 transition-colors duration-300 ease-in-out hover:bg-slate-200 sm:flex-row xl:mx-0"
-          >
-            <CloudImage
-              src={item.imgUrl}
-              alt={item.title}
-              height={500}
-              width={500}
-              // className="h-72 w-[450px] rounded-t-md object-cover opacity-95 transition-opacity duration-300 ease-in-out hover:opacity-100 sm:rounded-s-md sm:rounded-tr-none md:h-80"
-            />
-            <div className=" flex flex-col gap-y-2 pb-1 pl-1 sm:pl-8">
-              <p className="mt-4 text-sm">11/11/2023</p>
-              <h1 className="text-3xl font-semibold md:text-2xl">
-                {item.title}
-              </h1>
-              <p className=" md:text-sm lg:text-base">{item.desc}</p>
-            </div>
-          </Link>
-        ))}
-      {/* <BlogPostCard /> */}
-    </div>
-  )
-}
+//       {Array.isArray(data) &&
+//         data?.map((item: activitiesData) => (
+//           <Link
+//             key={item.id}
+//             href={'/blog/slug'}
+//             className="mx-4 flex flex-col justify-around rounded-md bg-slate-200/70 transition-colors duration-300 ease-in-out hover:bg-slate-200 sm:flex-row xl:mx-0"
+//           >
+//             <CloudImage
+//               src={item.imgUrl}
+//               alt={item.title}
+//               height={500}
+//               width={500}
+//               // className="h-72 w-[450px] rounded-t-md object-cover opacity-95 transition-opacity duration-300 ease-in-out hover:opacity-100 sm:rounded-s-md sm:rounded-tr-none md:h-80"
+//             />
+//             <div className=" flex flex-col gap-y-2 pb-1 pl-1 sm:pl-8">
+//               <p className="mt-4 text-sm">11/11/2023</p>
+//               <h1 className="text-3xl font-semibold md:text-2xl">
+//                 {item.title}
+//               </h1>
+//               <p className=" md:text-sm lg:text-base">{item.desc}</p>
+//             </div>
+//           </Link>
+//         ))}
+//     </div>
+//   )
+// }
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,16 +1,15 @@
-'use client'
-import AddAchievement from '@/components/AddAchievement'
-import AddActivity from '@/components/AddActivity'
-import AddSingleBlog from '@/components/AddSingleBlog'
+// import AddAchievement from '@/components/AddAchievement'
+// import AddActivity from '@/components/AddActivity'
+// import AddSingleBlog from '@/components/AddSingleBlog'
 
-export default function Dasboard() {
-  return (
-    <div className="mt-4 flex flex-col gap-y-4">
-      <AddSingleBlog />
-      <div className="flex flex-row items-center justify-center gap-x-6">
-        <AddAchievement />
-        <AddActivity />
-      </div>
-    </div>
-  )
-}
+// export default function Dasboard() {
+//   return (
+//     <div className="mt-4 flex flex-col gap-y-4">
+//       <AddSingleBlog />
+//       <div className="flex flex-row items-center justify-center gap-x-6">
+//         <AddAchievement />
+//         <AddActivity />
+//       </div>
+//     </div>
+//   )
+// }
