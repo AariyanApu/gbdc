b82a80166719f2c3654881e4bee5ee99db3e5edf
Aariyan Apu <38152118+AariyanApu@users.noreply.github.com>
2023-11-14 01:39:33 +0600

    fix bug

--- src/app/api/posts/[slug]/route.ts
+++ src/app/api/posts/[slug]/route.ts
@@ -1,10 +1,9 @@
 import prisma from '@/utils/connect'
-import { NextApiRequest } from 'next'
 import { NextResponse } from 'next/server'
 
 // Fetch single post
 export const GET = async (
-  req: NextApiRequest,
+  req: Request,
   { params }: { params: { slug: string } },
 ) => {
   const { slug } = params
