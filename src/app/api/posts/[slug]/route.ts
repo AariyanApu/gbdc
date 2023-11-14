81eaa036ce51aa5c1edd7cb4552256dd8046b84a
Aariyan Apu <38152118+AariyanApu@users.noreply.github.com>
2023-11-14 23:55:42 +0600

    added delete functionality

--- src/app/api/posts/[slug]/route.ts
+++ src/app/api/posts/[slug]/route.ts
@@ -29,3 +29,40 @@ export const GET = async (
     return new NextResponse('Database Error', { status: 500 })
   }
 }
+
+// Delete a post by slug
+export const DELETE = async (
+  req: Request,
+  { params }: { params: { slug: string } },
+) => {
+  const { slug } = params
+
+  try {
+    // Delete the post by slug
+    const deleteResult = await prisma.post.delete({
+      where: {
+        slug,
+      },
+    })
+
+    // Check if the post was successfully deleted
+    if (!deleteResult) {
+      return new NextResponse('Post not found', { status: 404 })
+    }
+
+    return new NextResponse(
+      JSON.stringify({ message: 'Post deleted successfully' }),
+    )
+  } catch (error) {
+    console.error('Error deleting post:', error)
+
+    // Log the complete error object
+    console.error('Complete error object:', error)
+
+    // Return a more detailed error response
+    return new NextResponse(
+      JSON.stringify({ error: 'Database Error', details: error }),
+      { status: 500 },
+    )
+  }
+}
--- src/components/DeletePost.tsx
+++ src/components/DeletePost.tsx
@@ -7,7 +7,7 @@ export default function DeletePost() {
     const res = await fetch(...args)
     return res.json()
   }
-  const { data, error }: SWRResponse<activitiesData, any> = useSWR(
+  const { data, error, mutate }: SWRResponse<activitiesData, any> = useSWR(
     '/api/posts',
     fetcher,
   )
@@ -15,7 +15,25 @@ export default function DeletePost() {
   if (!data) return <div>loading...</div>
   console.log(data)
 
-  const deletePost = () => {}
+  const deletePost = async (slug: string) => {
+    try {
+      const response = await fetch(`/api/posts/${slug}`, {
+        method: 'DELETE',
+      })
+
+      if (response.ok) {
+        const result = await response.json()
+        // setDeleteMessage(result.message)
+      } else {
+        const errorResult = await response.json()
+        console.error('Error deleting post:', errorResult)
+        // setDeleteMessage('Error deleting post')
+      }
+      mutate()
+    } catch (error) {
+      console.error('Error deleting post:', error)
+    }
+  }
 
   return (
     <div>
@@ -27,7 +45,7 @@ export default function DeletePost() {
           >
             <p>{item.title.substring(0, 60)}</p>
             <button
-              onClick={() => deletePost()}
+              onClick={() => deletePost(item.slug)}
               className=" rounded-md bg-red-500 px-4 py-2 text-white"
             >
               Delete
