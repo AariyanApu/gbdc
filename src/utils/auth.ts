ac1be2e701cc27a5764aae1884df7164f69d95e0
Aariyan Apu <38152118+AariyanApu@users.noreply.github.com>
2023-11-17 18:15:54 +0600

    added next-auth

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,1 +1,7 @@
+import { NextAuthOptions } from 'next-auth'
+import { CredentialsProvider } from 'next-auth/providers/credentials'
+import GoogleProvider from 'next-auth/providers/google'
 // export const getAuthSession = () => getServerSession(authOptions);
+export const authConfig: NextAuthOptions = {
+  providers: [],
+}
