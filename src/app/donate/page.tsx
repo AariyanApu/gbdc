336e848c3f05217231c33a5ae4afd3859ff108d3
Aariyan Apu <aariyan.apu@gmail.com>
2024-03-02 01:18:01 +0600

    create Donate page

---
+++ src/app/donate/page.tsx
@@ -0,0 +1,11 @@
+import Container from '@/components/Container'
+
+export default function Donate() {
+  return (
+    <Container>
+      <div className="mx-auto max-w-7xl ">
+        <h1>Donate</h1>
+      </div>
+    </Container>
+  )
+}
