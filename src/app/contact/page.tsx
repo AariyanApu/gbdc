6210362675ee8224f6978c53a0f67f25162364c8
Aariyan Apu <aariyan.apu@gmail.com>
2024-03-03 17:16:57 +0600

    update links

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -11,8 +11,18 @@ export default function Contact() {
           Head Office: Gournadi Blood Donor Club, Gournadi Bus Stand, Gournadi,
           Barisal.
         </p>
-        <p className="link_styles cursor-pointer">+8801918184091</p>
-        <p className="link_styles cursor-pointer">gbdc2018@gmail.com</p>
+        <Link
+          href={{ pathname: 'tel:+8801918184091' }}
+          className="link_styles cursor-pointer"
+        >
+          +8801918184091
+        </Link>
+        <Link
+          href={{ pathname: 'mailto:gbdc2018@gmail' }}
+          className="link_styles cursor-pointer"
+        >
+          gbdc2018@gmail.com
+        </Link>
         <Link
           className="link_styles"
           href={'https://www.facebook.com/groups/525407151272779'}
