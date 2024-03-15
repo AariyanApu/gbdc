6c1f01cb1180f8f2efc04ab37e0d540d922856d5
Aariyan Apu <aariyan.apu@gmail.com>
2024-03-16 00:51:41 +0600

    Added functionality to pagination Controll

--- src/components/PaginationControls.tsx
+++ src/components/PaginationControls.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import cn from '@/libs/cn'
 import { useRouter, useSearchParams, usePathname } from 'next/navigation'
 import { FC } from 'react'
 
@@ -21,8 +22,9 @@ const PaginationControls: FC<PaginationControlsProps> = ({
   return (
     <div className="mt-8 flex items-center justify-center gap-x-2 ">
       <button
-        className="rounded-lg bg-sky-400 px-4 py-2 text-white"
-        disabled={!hasPrevPage}
+        className={cn('rounded-lg bg-sky-400 px-4 py-2 text-white', {
+          hidden: !hasPrevPage,
+        })}
         onClick={() => {
           router.push(
             `${pathname}?page=${Number(page) - 1}&per_page=${per_page}`,
@@ -37,8 +39,9 @@ const PaginationControls: FC<PaginationControlsProps> = ({
       </div>
 
       <button
-        className="rounded-lg bg-sky-400 px-4 py-2 text-white"
-        disabled={!hasNextPage}
+        className={cn('rounded-lg bg-sky-400 px-4 py-2 text-white', {
+          hidden: !hasNextPage,
+        })}
         onClick={() => {
           router.push(
             `${pathname}?page=${Number(page) + 1}&per_page=${per_page}`,
