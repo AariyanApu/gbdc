f78449d2fc29cf6d7d3c9c3900c810fb68f038e4
Aariyan Apu <aariyan.apu@gmail.com>
2024-03-19 21:13:58 +0600

    fix font size of blog post

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -8,7 +8,7 @@ export default async function SingleBlog({ params }: any) {
 
   return (
     <div
-      className={`mx-auto my-4 max-w-5xl px-4 lg:px-0 ${banglaFont.className}`}
+      className={`mx-auto my-4 max-w-4xl px-4 lg:px-0 ${banglaFont.className}`}
     >
       <CloudImage
         src={data.imgUrl}
@@ -22,7 +22,7 @@ export default async function SingleBlog({ params }: any) {
         <p className="mt-8">{data.createdAt.substring(0, 10)}</p>
         <h1 className="text-2xl sm:text-3xl">{data.title} </h1>
         <div
-          className="md:text-sm lg:text-base"
+          className="pr-4 md:text-sm lg:text-lg "
           dangerouslySetInnerHTML={{
             __html: data.desc,
           }}
