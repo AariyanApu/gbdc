0094453977a1a9bd5c7e03694d317feabe3608cb
Aariyan Apu <aariyan.apu@gmail.com>
2024-03-23 23:43:37 +0600

    update donr section

--- src/components/DonorCard.tsx
+++ src/components/DonorCard.tsx
@@ -6,13 +6,15 @@ export default function DonorCard({ imgUrl }: { imgUrl: string }) {
   return (
     <MotionDiv
       variants={fadeIn('left', 'tween', 1.2, 0.7)}
-      customStyle="h-60 w-40 rounded-lg bg-slate-200 p-2 shadow  sm:h-72 sm:w-48"
+      customStyle=" relative h-60 w-40 rounded-lg bg-slate-200 p-2 shadow  sm:h-72 sm:w-48"
     >
       <Image
         src={imgUrl}
         alt="donors"
-        width={400}
-        height={400}
+        // width={400}
+        // height={400}
+        fill
+        priority
         className="h-full w-full rounded-xl bg-gray-900/5 object-cover shadow-lg "
       />
     </MotionDiv>
--- src/components/FeaturedCard.tsx
+++ src/components/FeaturedCard.tsx
@@ -1,23 +1,22 @@
-import Image from "next/image";
-import Link from "next/link";
+import Image from 'next/image'
+import Link from 'next/link'
 
 interface featuredCardProps {
-  imgUrl: string;
-  link: string;
+  imgUrl: string
+  link: string
 }
 export default function FeaturedCard({ imgUrl, link }: featuredCardProps) {
   return (
-    <div className=" h-24 w-72 cursor-pointer overflow-hidden rounded-lg bg-slate-200 p-2 drop-shadow-lg">
-      <Link href={link}>
+    <div className=" h-24 w-72 cursor-pointer overflow-hidden rounded-lg bg-slate-200 p-2 drop-shadow">
+      <Link href={link} className="h-[80px] w-72">
         <Image
           src={imgUrl}
-          alt="GBDC Featured on Sonali Somoy"
-          width={500}
-          height={500}
-          className="md h-[80px] w-72 rounded-xl object-fill
-           drop-shadow  grayscale filter transition-all duration-500 ease-in-out hover:scale-105 hover:grayscale-0"
+          alt="GBDC Featured on Newspaper"
+          fill
+          priority
+          className=" rounded-xl object-fill grayscale filter transition-all duration-500 ease-in-out hover:scale-105 hover:grayscale-0"
         />
       </Link>
     </div>
-  );
+  )
 }
