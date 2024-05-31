5de87f3bfb2eb979effd38684938b1d1d533e9fb
Aariyan Apu <aariyan.apu@gmail.com>
2024-05-31 17:54:35 +0600

    update fetching path

--- src/utils/getData.ts
+++ src/utils/getData.ts
@@ -1,12 +1,12 @@
 export const getData = async (id: string) => {
-  const res = await fetch(`https://gbdc.vercel.app/api/${id}`)
+  const res = await fetch(`https://www.gbdcbangladesh.com/api/${id}`)
   if (!res.ok) {
     throw new Error('Failed to fetch data')
   }
   return res.json()
 }
 export const getDataNoStore = async (id: string) => {
-  const res = await fetch(`https://gbdc.vercel.app/api/${id}`, {
+  const res = await fetch(`https://www.gbdcbangladesh.com/api/${id}`, {
     cache: 'no-store',
   })
   if (!res.ok) {
