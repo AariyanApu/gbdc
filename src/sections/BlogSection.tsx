c5bab3d0acc553a4f6479a10d13674922336551f
Aariyan Apu <aariyan.apu@gmail.com>
2024-06-17 02:11:25 +0600

    fix bug of fetching post

--- src/sections/BlogSection.tsx
+++ src/sections/BlogSection.tsx
@@ -2,11 +2,11 @@ import BlogPostCard from '@/components/BlogPostCard'
 import Container from '@/components/Container'
 import { TypingTitle } from '@/components/CustomText'
 import { activitiesData } from '@/types/randomTypes'
-import { getDataNoStore } from '@/utils/getData'
+import { getData, getDataNoStore } from '@/utils/getData'
 import React from 'react'
 
 export default async function BlogSection() {
-  const data: activitiesData[] = await getDataNoStore('posts')
+  const data: activitiesData[] = await getData('posts')
   const latestItem = data.reverse().slice(0, 1)
 
   return (
