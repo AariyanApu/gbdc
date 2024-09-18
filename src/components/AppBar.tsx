94cec30b665302b558665cf798cc1f1ea5ce60fa
Aariyan Apu <aariyan.apu@gmail.com>
2024-09-18 13:04:49 +0600

    fix bottom padding bug

--- src/components/AppBar.tsx
+++ src/components/AppBar.tsx
@@ -7,9 +7,9 @@ export default function AppBar() {
   const pathname = usePathname()
 
   return (
-    <div className="btm-nav z-50  px-2">
-      <div className="  flex h-[60px] flex-row justify-between rounded-box border border-primary px-8 shadow sm:h-24 lg:hidden">
-        {appBarItems.map(({ href, label, filledIcon, outlineIcon }) => (
+    <div className="btm-nav z-50 flex px-2  lg:hidden">
+      <div className="  flex h-[60px] flex-row justify-between rounded-box border border-primary px-8 shadow sm:h-24 ">
+        {appBarItems.map(({ href, filledIcon, outlineIcon }) => (
           <Link href={href} key={href} className="flex flex-col items-center">
             {pathname === href ? filledIcon : outlineIcon}
           </Link>
